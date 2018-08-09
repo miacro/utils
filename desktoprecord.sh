1dc59b7ab48af8a0108e8238958eaac36f0f46a6
miacro <fqguozhou@gmail.com>
2018-08-09 16:02:21 +0800

    ...

--- desktoprecord.sh
+++ desktoprecord.sh
@@ -3,4 +3,4 @@
 #  Name: alsa_output.pci-0000_01_00.1.hdmi-stereo.monitor
 #  Name: alsa_output.pci-0000_00_1b.0.analog-stereo.monitor # internal
 #  Name: alsa_input.pci-0000_00_1b.0.analog-stereo  # outside
-exec ffmpeg -f x11grab -s 1366x768  -i $DISPLAY  -f pulse -i default -r 25 test.mkv
+exec ffmpeg -f x11grab -s 1366x768 -framerate ntsc -i $DISPLAY -f pulse -i default -r ntsc test.mkv
