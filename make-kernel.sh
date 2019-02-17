236f7d30520995e18e1793b8bcecc80918f6989d
miacro <fqguozhou@gmail.com>
2019-02-17 10:55:02 +0800

    ...

--- make-kernel.sh
+++ make-kernel.sh
@@ -5,5 +5,5 @@ make install
 make modules_prepare
 make modules_install
 genkernel --install initramfs 
-grub-mkconfig -o /boot/efi/grub/grub.cfg
+grub-mkconfig -o /boot/grub/grub.cfg
 emerge -av @module-rebuild
