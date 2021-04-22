b4bc97b492b8bba4acf34f6254763a7eaca64e1a
miacro <fqguozhou@gmail.com>
2021-04-22 11:32:58 +0800

    update replace-string.sh

--- replace-string.sh
+++ replace-string.sh
@@ -1,59 +1,38 @@
-#!/bin/sh
-function replace()
-{
-  tmp_file=/tmp/replace_string.$$
-  sed "s/${2}/${3}/g" ${1} > ${tmp_file}
-  if [[ ! `cmp ${1} ${tmp_file}` == "" ]]
-  then
-    echo "processing file ${1}"
-    cat ${tmp_file} > ${1}
-  fi
-  rm ${tmp_file}
+#!/bin/bash
+function replace() {
+    tmp_file=/tmp/replace_string.$$
+    sed "s/${2}/${3}/g" "${1}" > ${tmp_file}
+    if [[ ! `cmp "${1}" ${tmp_file}` == "" ]]; then
+        echo "processing file \"${1}\""
+        cat ${tmp_file} > "${1}"
+    fi
+    rm ${tmp_file}
 }
 
-function usage()
-{
-  echo "usage: program [option] [pattern before] [pattern after] \
-    [dir or file name] "  
-  echo ""
-  echo "        option: -h for help"
-  echo "                -r recursion"
-  echo "                -n not recursion"
+function usage() {
+    echo "usage: program [option] [pattern before] [pattern after] \
+      [dir or file name] "
+    echo ""
+    echo "        option: -h for help"
+    echo "                -r recursion"
+    echo "                -n not recursion"
+    exit
 }
 
-file_list=""
-
-if [[ ${1} == "-h" ]]
-then
+if [[ ${1} == "-h" ]]; then
+  usage
+elif [[ $# !=  4 ]]; then
   usage
 fi
 
-if [[ $# !=  4 ]]
-then
-  usage
+find_args=""
+if [[ -d ${4} ]]; then
+    if [[ ${1} == "-n" ]]; then
+        file_args="-maxdepth 1"
+    fi
 fi
 
-if [[ -f ${4} ]]
-then
-  file_list=${4}
-fi
-
-if [[ -d ${4} ]]
-then
-  if [[ ${1} == "-n" ]]
-  then
-    file_list=`find ${4} -maxdepth 1`
-  fi
-  if [[ ${1} == "-r" ]]
-  then
-    file_list=`find ${4}`
-  fi
-fi
-
-for i in ${file_list}
+find "${4}" ${find_args} -type f -print0 | while read -d $'\0' file
 do
-  if [[ -f ${i} ]]
-  then
-    replace ${i} ${2} ${3}
-  fi
+  replace "${file}" ${2} ${3}
 done
