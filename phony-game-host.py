20eaab103b440b3a24bb73b6496ee2f396b9d454
miacro <fqguozhou@gmail.com>
2025-12-31 23:41:11 +0800

    add phony-game-host service

--- phony-game-host.py
+++ phony-game-host.py
@@ -6,7 +6,7 @@ UDP_IP = "0.0.0.0"
 UDP_PORT = 987
 
 
-def reply_always(host_type):
+def reply_always(host_type, verbose=False):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT))
 
@@ -21,15 +21,24 @@ running-app-name:Youtube\r
 running-app-titleid:CUSA01116\r
 \r
 """
+    count = 0
     while True:
         data, addr = sock.recvfrom(1024)
-        print(
-            "received message from %s[%d]: %s" % (addr[0], addr[1], data.decode("utf8"))
-        )
+        if verbose:
+            msg = "received message from %s[%d]: %s" % (
+                addr[0],
+                addr[1],
+                data.decode("utf8"),
+            )
+            print(msg)
 
         response = text.format(host_type, host_type, addr[1])
-        print(response)
+        if verbose:
+            print(response)
         sock.sendto(response.encode("utf8"), addr)
+        count += 1
+        if count % 1000 == 0:
+            print("received {} requests".format(count))
 
 
 def ask_response(ask_ip):
@@ -42,7 +51,7 @@ def ask_response(ask_ip):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Phony host type")
+    parser = argparse.ArgumentParser(description="Phony game host type")
     parser.add_argument(
         "--host-type",
         default="SteamDeck",
@@ -57,11 +66,15 @@ def main():
             "Switch 2",
         ],
     )
+    parser.add_argument(
+        "--verbose", default=False, action="store_true", help="enable verbose output"
+    )
+
     parser.add_argument("--ask-ip", default=None)
     args = parser.parse_args()
     if args.ask_ip is not None:
         ask_response(args.ask_ip)
-    reply_always(args.host_type)
+    reply_always(args.host_type, verbose=args.verbose)
 
 
 if __name__ == "__main__":
